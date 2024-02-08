68b37b208214035b328239d22be2f659a7ccb7e1
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-02-08 10:06:16 +0000

    Done EX01

--- chapter01/exercice01/index.js
+++ chapter01/exercice01/index.js
@@ -1,11 +1,13 @@
 
 export function greetWorld() {
-
+    return `Hey, world!`;
 }
 
 export function greet(name) {
+    return `Hey, ${name}!`;
 }
 
 export function capitilizedGreet(name) {
-
+    const capitalisedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    return `Hey, ${capitalisedName}!`;
 }
