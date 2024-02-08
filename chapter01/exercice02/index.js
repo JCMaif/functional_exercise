2f54063e5168b6cee9fcefd9cf3fa9565fd5a9ca
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-02-08 10:13:44 +0000

    Done EX02

--- chapter01/exercice02/index.js
+++ chapter01/exercice02/index.js
@@ -1,8 +1,20 @@
 export function sum(a, b) {
+    return a + b ;
 }
 
 export function isGreaterThan(input, comp) {
+    return input > comp ? true : false;
 }
 
 export function reverseNumber(num) {
+    return (
+        parseFloat(
+            num
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        )
+    );
+    
 }
